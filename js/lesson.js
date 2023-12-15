d2f3875d937535c4304c7501882f0143c733f4fa
Milana <kurmanalievamilana@MacBook-Pro-Milana.local>
2023-12-16 01:18:12 +0600

    dz3

---
+++ js/lesson.js
@@ -0,0 +1,64 @@
+
+
+const phoneInput = document.querySelector('#phone_input')
+const phoneButton = document.querySelector('#phone_button')
+const phoneResult = document.querySelector('#phone_result')
+
+const regExp = /^\+996 [25793]\d{2} \d{2}-\d{2}-\d{2}$/
+phoneButton.onclick = () => {
+    if (regExp.test(phoneInput.value)){
+        phoneResult.innerHTML = 'OK'
+        phoneResult.style.color = 'green'
+    }else {
+        phoneResult.innerHTML = 'NOT OK'
+        phoneResult.style.color = 'red'
+    }
+}
+
+const tabContentBlocks = document.querySelectorAll('.tab_content_block')
+const tabs = document.querySelectorAll('.tab_content_item')
+const tabsParent = document.querySelector('.tab_content_items')
+
+const hideTabContent = ()=>{
+    tabContentBlocks.forEach(tabCard =>{
+        tabCard.style.display = 'none'
+    })
+    tabs.forEach(tab =>{
+        tab.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabContent = (tabIndex = 0)=>{
+    tabContentBlocks[tabIndex].style.display = 'block'
+    tabs[tabIndex].classList.add('tab_content_item_active')
+}
+
+hideTabContent()
+showTabContent()
+
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')){
+        tabs.forEach((tab, tabIndex) => {
+            if (event.target === tab){
+                hideTabContent()
+                showTabContent(tabIndex)
+            }
+        })
+    }
+}
+
+const tabsAuto = (i = 0) => {
+    setInterval(() => {
+        i++
+        if (i > tabContentBlocks.length - 1){
+            i = 0
+        }
+        hideTabContent()
+        showTabContent(i)
+    }, 3000)
+}
+tabsAuto()
+
+setTimeout(()=> openModal(), 10000)
+
+scrollEnd()
